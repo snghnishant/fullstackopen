bed7501ba94938edbc8c6cc2a5abff88225a59b6
snghnishant <blackmalware@protonmail.com>
2020-11-30 11:47:37 +0530

    Exercise 1.6

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () =>{
+    // save clicks of each button to its own state
+    const [good, setGood] = React.useState(0);
+    const [neutral, setNeutral] = React.useState(0);
+    const [bad, setBad] = React.useState(0);
+    
     return(
         <div>
             Hello World
