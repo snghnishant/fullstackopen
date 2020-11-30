b6ceface346768a8ccd56ee1537a9eeb2154c984
snghnishant <blackmalware@protonmail.com>
2020-11-30 14:11:17 +0530

    Exercise 1.12 Random quote button

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () =>{
+const Button = ({getRandom}) =>{
   return (<div>
-    Hello World
+    <button onClick={getRandom}>Next Anecdote</button>
   </div>);
 }
+const App = (props) => {
+  const [selected, setSelected] = React.useState(0)
+
+  const getRandom = () =>{
+    let randomNumber = Math.floor(Math.random()* Math.floor(anecdotes.length));
+    console.log(randomNumber);
+    setSelected(randomNumber);
+  }
+
+  return (
+    <div>
+      <q>{props.anecdotes[selected]}</q>
+      <Button getRandom={getRandom}/>
+    </div>
+  )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App anecdotes={anecdotes}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
