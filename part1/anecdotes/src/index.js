355bad157626779d6ad4b838c538d414ed118914
snghnishant <blackmalware@protonmail.com>
2020-12-10 22:07:28 +0530

    Exercise 1.13 added voteup

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,28 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Button = ({getRandom}) =>{
-  return (<div>
-    <button onClick={getRandom}>Next Anecdote</button>
-  </div>);
-}
-const App = (props) => {
-  const [selected, setSelected] = React.useState(0)
-
-  const getRandom = () =>{
-    let randomNumber = Math.floor(Math.random()* Math.floor(anecdotes.length));
-    console.log(randomNumber);
-    setSelected(randomNumber);
-  }
-
-  return (
-    <div>
-      <q>{props.anecdotes[selected]}</q>
-      <Button getRandom={getRandom}/>
-    </div>
-  )
-}
-
 const anecdotes = [
   'If it hurts, do it more often',
   'Adding manpower to a late software project makes it later!',
@@ -30,12 +8,53 @@ const anecdotes = [
   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
   'Premature optimization is the root of all evil.',
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+];
+
+const voteArray = new Array(anecdotes.length).fill(0);
+
+const Buttons = ({getRandom, voteUp}) =>{
+  return (<div>
+    <button onClick={voteUp}>Vote</button>
+    <button onClick={getRandom}>Next Anecdote</button>
+  </div>);
+}
+const App = (props) => {
+  const [selected, setSelected] = React.useState(0);
+  const [votes, setVotes] = React.useState(voteArray);
+  const [currentIndex, setIndex] = React.useState(0);
+
+  const getRandom = () =>{
+    let randomNumber = Math.floor(Math.random()* Math.floor(anecdotes.length));
+    console.log(randomNumber);
+    setSelected(randomNumber);
+    setIndex(randomNumber);
+  }
+
+  const voteUp = () =>{
+    const newVotes = [...votes];
+    newVotes[currentIndex]+=1;
+    console.log(currentIndex, newVotes);
+    setVotes(newVotes);
+  }
+
+  return (
+    <div>
+      <div>
+        <h1>Anecdote of the day</h1>
+        <p>{props.anecdotes[selected]}</p>
+        <p>has {votes[currentIndex]} votes</p>
+      </div> 
+      <Buttons getRandom={getRandom} voteUp={voteUp}/>
+    </div>
+  )
+}
+
+
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App anecdotes={anecdotes}/>
+    <App anecdotes={anecdotes} votes={voteArray}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
