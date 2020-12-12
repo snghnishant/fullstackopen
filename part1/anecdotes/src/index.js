16b4a04c767974fc9e6046b10a9aee34f7b57ffd
snghnishant <blackmalware@protonmail.com>
2020-12-12 10:57:25 +0530

    Exercise 1.14 added mostvoted function, refactored

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// anecdotes array accessible globally
 const anecdotes = [
   'If it hurts, do it more often',
   'Adding manpower to a late software project makes it later!',
@@ -10,41 +11,67 @@ const anecdotes = [
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ];
 
+// voteArray with initial values set to 0
 const voteArray = new Array(anecdotes.length).fill(0);
 
+// Button components
 const Buttons = ({getRandom, voteUp}) =>{
   return (<div>
     <button onClick={voteUp}>Vote</button>
     <button onClick={getRandom}>Next Anecdote</button>
   </div>);
 }
+
+// MostVoted component with conditional rendering to only show mostVoted quote when available
+const MostVoted = ({mostVotes, votes}) =>{
+  if(mostVotes === null)
+    return null;
+  
+  return(
+    <div>
+      <h1>Anecdote with most votes</h1>
+      <h4>{anecdotes[mostVotes]}</h4>
+      <h5>has {votes[mostVotes]} votes</h5>
+    </div>
+  )
+}
+
+// App component
 const App = (props) => {
   const [selected, setSelected] = React.useState(0);
   const [votes, setVotes] = React.useState(voteArray);
-  const [currentIndex, setIndex] = React.useState(0);
+  const [currentIndex, setIndex] = React.useState(0); // to store index state 
+  const [mostVotes, setMostVoted] = React.useState(null);
 
+    // random quote picker event handler function
   const getRandom = () =>{
-    let randomNumber = Math.floor(Math.random()* Math.floor(anecdotes.length));
+    const randomNumber = Math.floor(Math.random()* Math.floor(anecdotes.length));
     console.log(randomNumber);
     setSelected(randomNumber);
-    setIndex(randomNumber);
+    setIndex(randomNumber); //changing index state to the randomNumber index generated from above
+    
   }
 
+  // vote up event handler
   const voteUp = () =>{
     const newVotes = [...votes];
     newVotes[currentIndex]+=1;
     console.log(currentIndex, newVotes);
     setVotes(newVotes);
+    const mostVotedIndex = newVotes.indexOf(Math.max(...newVotes));
+    console.log(mostVotedIndex);
+    setMostVoted(mostVotedIndex);
   }
 
   return (
     <div>
       <div>
         <h1>Anecdote of the day</h1>
-        <p>{props.anecdotes[selected]}</p>
-        <p>has {votes[currentIndex]} votes</p>
+        <h4>{props.anecdotes[selected]}</h4>
+        <h5>has {votes[currentIndex]} votes</h5>
       </div> 
       <Buttons getRandom={getRandom} voteUp={voteUp}/>
+      <MostVoted mostVotes={mostVotes} votes={votes}/>
     </div>
   )
 }
