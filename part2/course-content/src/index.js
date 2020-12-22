800a0bae7dc60c9fca5a3d241bd9be8a85ba86de
snghnishant <blackmalware@protonmail.com>
2020-12-22 18:26:25 +0530

    Exercise 2.5

--- part2/course-content/src/index.js
+++ part2/course-content/src/index.js
@@ -1,61 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Course from "./Components/course";
-
-const App = () => {
-	const courses = [
-		{
-			id: 1,
-			name: "Half Stack application development",
-			parts: [
-				{
-					name: "Fundamentals of React",
-					exercises: 10,
-					id: 1,
-				},
-				{
-					name: "Using props to pass data",
-					exercises: 7,
-					id: 2,
-				},
-				{
-					name: "State of a component",
-					exercises: 14,
-					id: 3,
-				},
-				{
-					name: "Redux",
-					exercises: 11,
-					id: 4,
-				},
-			],
-		},
-		{
-			name: "Node.js",
-			id: 2,
-			parts: [
-				{
-					name: "Routing",
-					exercises: 3,
-					id: 1,
-				},
-				{
-					name: "Middlewares",
-					exercises: 7,
-					id: 2,
-				},
-			],
-		},
-	];
-
-	return (
-		<div>
-			{courses.map((course) => (
-				<Course key={course.id} course={course} />
-			))}
-		</div>
-	);
-};
+import App from "./App";
 
 ReactDOM.render(
 	<React.StrictMode>
