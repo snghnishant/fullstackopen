8d1436a5996d2d1bb8d3437c4a202af5fae7f337
snghnishant <blackmalware@protonmail.com>
2020-12-22 18:22:40 +0530

    Exercise 2.4

--- part2/course-content/src/index.js
+++ part2/course-content/src/index.js
@@ -3,30 +3,58 @@ import ReactDOM from "react-dom";
 import Course from "./Components/course";
 
 const App = () => {
-	const course = {
-		id: 1,
-		name: "Half Stack application development",
-		parts: [
-			{
-				name: "Fundamentals of React",
-				exercises: 10,
-			},
-			{
-				name: "Using props to pass data",
-				exercises: 7,
-			},
-			{
-				name: "State of a component",
-				exercises: 14,
-			},
-			{
-				name: "Redux",
-				exercises: 11,
-			},
-		],
-	};
+	const courses = [
+		{
+			id: 1,
+			name: "Half Stack application development",
+			parts: [
+				{
+					name: "Fundamentals of React",
+					exercises: 10,
+					id: 1,
+				},
+				{
+					name: "Using props to pass data",
+					exercises: 7,
+					id: 2,
+				},
+				{
+					name: "State of a component",
+					exercises: 14,
+					id: 3,
+				},
+				{
+					name: "Redux",
+					exercises: 11,
+					id: 4,
+				},
+			],
+		},
+		{
+			name: "Node.js",
+			id: 2,
+			parts: [
+				{
+					name: "Routing",
+					exercises: 3,
+					id: 1,
+				},
+				{
+					name: "Middlewares",
+					exercises: 7,
+					id: 2,
+				},
+			],
+		},
+	];
 
-	return <Course course={course} />;
+	return (
+		<div>
+			{courses.map((course) => (
+				<Course key={course.id} course={course} />
+			))}
+		</div>
+	);
 };
 
 ReactDOM.render(
