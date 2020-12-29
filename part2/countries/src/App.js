48fd483d906139eb165bcaf653065c6d7fcfbc8c
snghnishant <blackmalware@protonmail.com>
2020-12-29 19:50:34 +0530

    initial commit

---
+++ part2/countries/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import axios from "axios";
+import "./App.css";
+
+const App = () => {
+	return (
+		<div>
+			<div>
+				Find Countries: <input />
+			</div>
+		</div>
+	);
+};
+
+export default App;
