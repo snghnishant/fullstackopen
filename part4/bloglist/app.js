383937424adf1b7d35bcf62b796fec51b9d03dde
snghnishant <blackmalware@protonmail.com>
2021-01-25 22:12:29 +0530

    Exercise 4.1, 4.2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+const app = express();
+const Blog = require("./models/blog");
+const blogRouter = require("./controllers/blog.js");
+const middleware = require("./utils/middleware");
+
+// Connection to DB
+const url = process.env.MONGODB_URI;
+console.log("Connecting to database...");
+mongoose
+	.connect(url, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+		useCreateIndex: true,
+	})
+	.then(() => {
+		console.log("Connection successful!");
+	})
+	.catch((error) => {
+		console.error("Error connecting to DB", error.message);
+	});
+
+// Using Middleware
+app.use(cors());
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use("/api", blogRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
---
+++ part4/bloglist/utils/middleware.js
@@ -0,0 +1,19 @@
+const unknownEndpoint = (req, res) => {
+	res.status(404).send({ error: "Unknown endpoint" });
+};
+
+const errorHandler = (error, req, res, next) => {
+	console.log(error.message);
+
+	if (error.name === "CastError") {
+		return res.status(400).send({ error: "Malformatted ID" });
+	} else if (error.name === "ValidationError") {
+		return res.status(400).send({ error: error.message });
+	}
+	next(error);
+};
+
+module.exports = {
+	unknownEndpoint,
+	errorHandler,
+};
